2be797c7e5a3d6fd16c8ec348fdaedf6667c6c59
TheRealKoeDev <koerner.jena@web.de>
2021-02-17 23:55:12 +0100

    fix: minor spelling mistake

--- src/form-builder/build-type.ts
+++ src/form-builder/build-type.ts
@@ -1,12 +1,12 @@
 import 'reflect-metadata'
 
 import { Type, TypeHelpOptions, TypeOptions } from "class-transformer";
-import { Type as TypeConstructos } from '@angular/core';
+import { Type as TypeConstructor } from '@angular/core';
 import { defaultTypeStore } from './type-store';
 
 export function BuildType(typeFunction?: (type?: TypeHelpOptions) => Function, options?: TypeOptions): PropertyDecorator {
     return function (target: Object, propertyKey: string | symbol){
-        const ParentConstructor = target.constructor as TypeConstructos<unknown>;
+        const ParentConstructor = target.constructor as TypeConstructor<unknown>;
 
         const newObject = new ParentConstructor();
         const typeHelpOptions: TypeHelpOptions = {
@@ -15,7 +15,7 @@ export function BuildType(typeFunction?: (type?: TypeHelpOptions) => Function, o
             property: propertyKey?.toString(),
         }
 
-        const ChildType = typeFunction(typeHelpOptions) as TypeConstructos<unknown>;
+        const ChildType = typeFunction(typeHelpOptions) as TypeConstructor<unknown>;
         defaultTypeStore.registerType(ParentConstructor, propertyKey?.toString(), ChildType);
 
         Type(() => ChildType, options)(target, propertyKey);
